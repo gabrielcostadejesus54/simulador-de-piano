6b97026d44d8431840ce348b782b00c4520eb310
Gabriel Costa <gabrielcostadeveloper@gmail.com>
2024-01-10 15:37:36 -0300

    Versão 1.0 Finaizada

---
+++ src/scripts/engine.js
@@ -0,0 +1,45 @@
+const teclasPiano = document.querySelectorAll(".piano-teclas .tecla")
+let audio = new Audio(`src/audios/a.wav`)
+let mapeamentoTecla = []
+const volume = document.querySelector(".volume input")
+const verTeclas = document.querySelector('.ver-teclas input')
+
+const playTune = function(tecla){
+    audio.src = `src/audios/${tecla}.wav`
+    audio.play()
+
+    const clickTecla = document.querySelector(`[data-tecla="${tecla}"]`)
+    clickTecla.classList.add("active")
+
+    setTimeout(() => {
+        clickTecla.classList.remove('active')
+    },150)
+   
+}
+
+teclasPiano.forEach((tecla) => {
+    console.log(tecla.dataset.tecla)
+    tecla.addEventListener('click', () => playTune(tecla.dataset.tecla))
+    mapeamentoTecla.push(tecla.dataset.tecla)
+    console.log(mapeamentoTecla)
+})
+
+document.addEventListener('keydown', (e) => {
+    if(mapeamentoTecla.includes(e.key)){
+        playTune(e.key)
+    }
+})
+
+function handleVolume(e){
+    audio.volume = e.target.value
+}
+
+function showHideTeclas(){
+     teclasPiano.forEach((tecla) => {
+        tecla.classList.toggle('hide')
+     })
+}
+
+volume.addEventListener('input', handleVolume)
+
+verTeclas.addEventListener('click', showHideTeclas )
